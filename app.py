e792d1dcf69adaff9767241fb7de435829aa52bd
elabs <elijahknsubuga@gmail.com>
2021-09-16 08:41:50 +0300

    changed  hte client from imperial to metric

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import python_weather
 import asyncio
 
 async def getweather():
-    client = python_weather.Client(format=python_weather.IMPERIAL)
+    client = python_weather.Client(format=python_weather.METRIC)
 
     city = input("Please Enter City: ")
     weather = await client.find(city)
 
     print("The current temperature is: ", weather.current.temperature)
     print("The current humidity is: ", weather.current.humidity)
-    #print(weather.current.pressure)
 
     for forecast in weather.forecasts:
-        print("The temperature on the is/was" + str(forecast.date) + " is: ",
+        print("The temperature on the " + str(forecast.date) + " is/was:",
               forecast.sky_text, forecast.temperature)
 
     await client.close()
