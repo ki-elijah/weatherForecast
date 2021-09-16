2b726a8b75bba9b49cb7150cf76167f4b17086fb
elabs <elijahknsubuga@gmail.com>
2021-09-16 08:39:23 +0300

    altered the text code because it was giving me errors

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-import requests, json
+import python_weather
+import asyncio
 
+async def getweather():
+    client = python_weather.Client(format=python_weather.IMPERIAL)
 
+    city = input("Please Enter City: ")
+    weather = await client.find(city)
 
+    print("The current temperature is: ", weather.current.temperature)
+    print("The current humidity is: ", weather.current.humidity)
+    #print(weather.current.pressure)
 
-api_key = "eab568a3c8c70faba0d92c3277f3339b"
+    for forecast in weather.forecasts:
+        print("The temperature on the is/was" + str(forecast.date) + " is: ",
+              forecast.sky_text, forecast.temperature)
 
-base_url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={api_key}"
+    await client.close()
 
-city_name = input("Enter city name: ")
-complete_url = base_url + "524901" + api_key + city_name
-response = requests.get(complete_url)
-x = response.json()
-if x["cod"] != "404":
-    y = x['main']
-    current_temperature = y["temp"]
-    current_pressure = y["pressure"]
-    current_humidity = y["humidity"]
-    z = x["weather"]
-    weather_description = z[0]["weather_description"]
-    print("Temperature (in Kelvin) = " + str(current_temperature) + 
-          "\n atmospheric pressure (in hPa) = " + str(current_pressure) + 
-          "\n humidity (in percentage) = " + str(current_humidity) + 
-          "\n description = " + str(weather_description))
-else:
-    print("City not found")
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getweather())
