d1b7e8e2fce45af60c873846cb45d7edc1caa622
elabs <elijahknsubuga@gmail.com>
2021-09-16 08:55:04 +0300

    added more attributes like humidity, windspeed and some alterations

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ async def getweather():
     client = python_weather.Client(format=python_weather.METRIC)
 
     city = input("Please Enter City: ")
+    print('****************************************************************')
+    print("+++++" + city + "+++++")
     weather = await client.find(city)
 
+    print("Date: ", weather.current.date)
     print("The current temperature is: ", weather.current.temperature)
     print("The current humidity is: ", weather.current.humidity)
+    print("The current wind speed is: ", weather.current._get('@windspeed'))
+    print("Feels like: ", weather.current.feels_like)
 
     for forecast in weather.forecasts:
         print("The temperature on the " + str(forecast.date) + " is/was:",
